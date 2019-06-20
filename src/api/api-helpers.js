c644527fff023836523a185c7be8d558161d431f
klequis <klequis@gmail.com>
2019-06-20 09:12:54 -0700

    fetch using async/await

--- src/api/api-helpers.js
+++ src/api/api-helpers.js
@@ -9,31 +9,62 @@ const logRequest = (url, options, headers) => {
   console.groupEnd()
 }
 
-export const fetchJson = (url, options = {}) => {
+export const fetchJson = async (url, options = {}) => {
   let headers = {
     ...options.headers,
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   }
   logRequest(url, options, headers)
-  return (
-    fetch(url, {
-      ...options,
-      headers,
-    }).then(res => {
-      const { status } = res
-      if (status >= 200 && status < 300) {
-        return res.json()
-      } else {
-        const err = {
-          status: res.status,
-          statusText: res.statusText,
-          url: res.url,
-        }
-        throw err
-      }
-    })
-  )
+
+  const r1 = await fetch(url, {
+    ...options,
+    headers,
+  })
+
+  orange('r1', r1)
+  
+
+  const { status } = r1
+
+  if (status >= 200 && status < 300) {
+    return await r1.json()
+  } else {
+    const err = {
+      status: r1.status,
+      statusText: r1.statusText,
+      url: r1.url,
+    }
+    throw err
+  }
+    
 }
 
 export default { fetchJson }
+
+// export const fetchJson = (url, options = {}) => {
+//   let headers = {
+//     ...options.headers,
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json',
+//   }
+//   logRequest(url, options, headers)
+//   return (
+//     fetch(url, {
+//       ...options,
+//       headers,
+//     }).then(res => {
+//       const { status } = res
+//       if (status >= 200 && status < 300) {
+//         return res.json()
+//       } else {
+//         const err = {
+//           status: res.status,
+//           statusText: res.statusText,
+//           url: res.url,
+//         }
+//         throw err
+//       }
+//     })
+//   )
+// }
